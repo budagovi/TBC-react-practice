a9514e533a6d73617691cfc2247a200e2d6c7825
budagovi <lukabudagovi@gmail.com>
2024-05-17 17:41:12 +0400

    fixed user crud

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const AUTH_COOKIE_KEY = 'auth';
 export const CART_LOCSTORE_KEY = 'cart';
-export const BASE_URL = process.env.VERCEL_ENV === "development" ? "http://localhost:3000" : process.env.VERCEL_URL;
+export const BASE_URL = process.env.VERCEL_ENV === "development" ? "http://localhost:3000" : "https://tbc-react-six.vercel.app";
