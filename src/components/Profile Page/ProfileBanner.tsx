2049a1f6defc600d2631b17cb44da121756e6907
budagovi <lukabudagovi@gmail.com>
2024-06-26 15:39:08 +0400

    avatar upload

--- src/components/Profile Page/ProfileBanner.tsx
+++ src/components/Profile Page/ProfileBanner.tsx
@@ -1,9 +1,14 @@
+'use client'
 // --- style
 import style from './ProfileBanner.module.css';
 // --- types
 import type { IUser } from '@/src/lib/types/entities';
 // --- next api
 import { redirect } from 'next/navigation';
+
+import { type PutBlobResult } from '@vercel/blob';
+import { upload } from '@vercel/blob/client';
+import { useState, useRef } from 'react';
 
 interface IProps {
   user: IUser
@@ -12,17 +17,41 @@ interface IProps {
 /**
  * Banner of the all Profile route segments (image and fullname)
  */
-const ProfileBanner = async ({ user }: IProps) => {
+const ProfileBanner = ({ user }: IProps) => {
 
-  const imgPlaceholder = user.image ? user.image : "https://st3.depositphotos.com/13159112/17145/v/450/depositphotos_171453724-stock-illustration-default-avatar-profile-icon-grey.jpg"
-  if(!user)
+  if (!user)
     redirect('/sign-in')
 
+  const inputFileRef = useRef<HTMLInputElement>(null);
+  const [blob, setBlob] = useState<PutBlobResult | null>(null);
+
+  const imgPlaceholder = blob?.url ? blob.url : "https://st3.depositphotos.com/13159112/17145/v/450/depositphotos_171453724-stock-illustration-default-avatar-profile-icon-grey.jpg"
   return (
     <div className={style.wrapper}>
       <div className={style.avatarWrapper}>
         <img src={imgPlaceholder} alt="avatar" />
       </div>
+      <form
+        onSubmit={async (event) => {
+          event.preventDefault();
+
+          if (!inputFileRef.current?.files) {
+            throw new Error('No file selected');
+          }
+
+          const file = inputFileRef.current.files[0];
+
+          const newBlob = await upload(file.name, file, {
+            access: 'public',
+            handleUploadUrl: '/api/avatar/upload',
+          });
+
+          setBlob(newBlob);
+        }}
+      >
+        <input name="file" ref={inputFileRef} type="file" required />
+        <button type="submit">Upload</button>
+      </form>
       <div className={style.text}>
         <span>{user.lastname}  {user.firstname}</span>
         <p>Your account is ready, {user.email}</p>
